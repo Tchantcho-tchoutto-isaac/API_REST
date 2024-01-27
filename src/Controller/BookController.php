67c4f434486c3e169f0e75ce65f53232ba1df1ef
Tchantcho-tchoutto-isaac <94875081+Tchantcho-tchoutto-isaac@users.noreply.github.com>
2024-01-27 23:27:13 +0100

    utilisarion d'extra bundel

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,17 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
 use Symfony\Component\HttpFoundation\Response;
 use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
-
 class BookController extends AbstractController
 {
     #[Route('/api/books', name: 'book', methods: ['GET'])]
-    public function index(BookRepository $bookRepository,SerializerInterface $serializer): JsonResponse
+    public function getBooks(BookRepository $bookRepository,SerializerInterface $serializer): JsonResponse
     {
         $books= $bookRepository->findAll();
         $bookList= $serializer->serialize($books,'json');
@@ -24,5 +24,27 @@ class BookController extends AbstractController
                true
         );
 
+    }
+
+    /*
+    #[Route('/api/books/{id}', name: 'detail', methods: ['GET'])]
+    public function getBookdetail( int $id , BookRepository $bookRepository,SerializerInterface $serializer): JsonResponse
+    {
+        $books= $bookRepository->find($id);
+        if ($books) {
+
+        $bookList= $serializer->serialize($books,'json');
+        return new JsonResponse($bookList,Response::HTTP_OK, [],true);
+
+        return new JsonResponse(null , Response::HTTP_NOT_FOUND );
+         }
+       
+    }*/
+
+    #[Route('/api/books/{id}', name: 'detailBook', methods: ['GET'])]
+    public function getDetailBook(Book $book, SerializerInterface $serializer): JsonResponse 
+    {
+        $jsonBook = $serializer->serialize($book, 'json');
+        return new JsonResponse($jsonBook, Response::HTTP_OK, ['accept' => 'json'], true);
     }
 }
